2ee7f0147dfc21c57e2416d33e787b097cc73aa8
Umesh R Bilagi <umeshbilagi@gmail.com>
2018-03-17 07:57:50 +0530

    write cookie string

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require("http");
 const https = require("https");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
 
 /*
@@ -17,18 +18,52 @@ const options = {
 }
 
 http.createServer(function(req, res){
+    var cookieString="";
     if(req.url=="/"){
+        res.writeHead(200,{
+            'Set-Cookie' : cookieString,
+            'Content-Type' : 'text/plain'
+        })
+        res.end("Set http cookie");
 
     }else if(req.url == "/getCookie"){
+        getCookie("cookieHTTP")
 
     }
 
 }).listen(9000);
 
 https.createServer(options, function(req, res){
+    var cookieString="";
     if(req.url=="/"){
-
+        res.writeHead(200,{
+            'Set-Cookie' : cookieString,
+            'Content-Type' : 'text/plain'
+        })
+        res.end("Set http cookie");
     }else if(req.url == "/getCookie"){
+        getCookie("cookieHTTPS");
 
     }
 }).listen(8000);
+
+
+
+var getCookie = (curCookie)=>{
+    var cookies = req.headers.cookie;
+    if(util.isUndefined(cookies)){
+        return  "";
+    }else{
+        var cookieArray = cookies.split(";");
+        for (let index = 0; index < cookieArray.length; index++) {
+           
+            var curJson = queryString.parse(cookieArray[index]);
+            var key = Object.keys(curJson);
+            if (key[0].trim() == curCookie){
+            return curJson[key[0]];           
+                
+            }
+        }
+        
+    }
+}
